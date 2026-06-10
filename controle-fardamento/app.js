ad425842139ead4808eac59e38c7e7c04396cc25
GuilhermeSI <Luiz.orestes@sou.unifeb.edu.br>
2026-06-09 22:37:31 -0300

    Voltar para __dirname nas views

--- controle-fardamento/app.js
+++ controle-fardamento/app.js
@@ -9,7 +9,7 @@ const PORT = 3000;
 
 // Configurações do Express
 app.set('view engine', 'ejs');
-app.set('views', path.join(process.cwd(), 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
